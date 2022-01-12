06945f3877777f8a9ede50fb48f62e4c7b45e7d1
Gary Hopkins <gary287@gmail.com>
2022-01-11 22:00:22 -0500

    add basic url validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :list
   validates :title, presence: true
+  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp }, if: -> { url.present? }
+  validates :url, format: { with: %r{https?://.+} }, if: -> { url.present? }
 end
